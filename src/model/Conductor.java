7f6881f243adea88d20128f632ae4929f0a2d0e6
fabriciojosesono10-star <fabriciojosesono10@gmail.com>
2025-11-09 22:31:43 +0000

    feat: Implementation of the Driver class

---
+++ src/model/Conductor.java
@@ -0,0 +1,15 @@
+package bombersystem.model;
+
+public class Conductor {
+    private String nombre;
+    private String licencia;
+
+    public Conductor(String nombre, String licencia) {
+        this.nombre = nombre;
+        this.licencia = licencia;
+    }
+
+    public void mostrarConductor() {
+        System.out.println("Conductor: " + nombre + " - Licencia: " + licencia);
+    }
+}
