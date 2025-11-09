862050fab8356c21cba5d3b1cf85059ec2d7d43c
fabriciojosesono10-star <fabriciojosesono10@gmail.com>
2025-11-09 22:30:51 +0000

    feat: implementation of the EmergencyUnit class

---
+++ src/model/UnidadEmergencia.java
@@ -0,0 +1,15 @@
+package bombersystem.model;
+
+public class UnidadEmergencia {
+    private String codigo;
+    private String tipo;
+
+    public UnidadEmergencia(String codigo, String tipo) {
+        this.codigo = codigo;
+        this.tipo = tipo;
+    }
+
+    public void mostrarInfo() {
+        System.out.println("Unidad " + codigo + " - Tipo: " + tipo);
+    }
+}
