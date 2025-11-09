74b5b6630ba06840c738f4acb22fc2780f732849
fabriciojosesono10-star <fabriciojosesono10@gmail.com>
2025-11-09 22:29:53 +0000

    feat: implementation of the FireStation class

---
+++ src/model/EstacionBomberos.java
@@ -0,0 +1,24 @@
+package bombersystem.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EstacionBomberos {
+    private String nombre;
+    private String direccion;
+    private List<UnidadEmergencia> unidades = new ArrayList<>();
+
+    public EstacionBomberos(String nombre, String direccion) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+
+    public void agregarUnidad(UnidadEmergencia unidad) {
+        unidades.add(unidad);
+    }
+
+    public void mostrarDatos() {
+        System.out.println("Estación: " + nombre + " - Dirección: " + direccion);
+        System.out.println("Unidades asignadas: " + unidades.size());
+    }
+}
