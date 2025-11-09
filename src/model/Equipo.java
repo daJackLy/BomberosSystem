db425d3cfbef7814096465d0ff680f91b101527b
fabriciojosesono10-star <fabriciojosesono10@gmail.com>
2025-11-09 22:32:23 +0000

    feat: implementation of the Team class

---
+++ src/model/Equipo.java
@@ -0,0 +1,15 @@
+package bombersystem.model;
+
+public class Equipo {
+    private String nombre;
+    private int cantidad;
+
+    public Equipo(String nombre, int cantidad) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+    }
+
+    public void mostrarEquipo() {
+        System.out.println(nombre + " (Cantidad: " + cantidad + ")");
+    }
+}
