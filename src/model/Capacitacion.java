7841c0f6ec0d353b295115195fbf749e07ffbcfd
fabriciojosesono10-star <fabriciojosesono10@gmail.com>
2025-11-09 22:36:32 +0000

    feat: Implementation of the Training class

---
+++ src/model/Capacitacion.java
@@ -0,0 +1,15 @@
+package bombersystem.model;
+
+public class Capacitacion {
+    private String tema;
+    private String fecha;
+
+    public Capacitacion(String tema, String fecha) {
+        this.tema = tema;
+        this.fecha = fecha;
+    }
+
+    public void mostrarCapacitacion() {
+        System.out.println("Capacitación: " + tema + " (" + fecha + ")");
+    }
+}
