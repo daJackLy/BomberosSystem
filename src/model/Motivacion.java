3358a6e3b6c71f7ebac5cccc20442584ddf4c2cd
fabriciojosesono10-star <fabriciojosesono10@gmail.com>
2025-11-24 13:12:25 -0500

    Update Motivacion.java

--- src/model/Motivacion.java
+++ src/model/Motivacion.java
@@ -1,31 +1,22 @@
 package model;
 
 public class Motivacion {
-
     private String preguntaMotivacion;
     private String preguntaAporte;
     private String preguntaPresion;
-    private String horarioColaborar;
+    private boolean participacionCapacitacionesIniciales;
 
-    public Motivacion() {}
-
-    public Motivacion(String preguntaMotivacion, String preguntaAporte, String preguntaPresion, String horarioColaborar) {
+    public Motivacion(String preguntaMotivacion, String preguntaAporte, String preguntaPresion,
+                      boolean participacionCapacitacionesIniciales) {
         this.preguntaMotivacion = preguntaMotivacion;
         this.preguntaAporte = preguntaAporte;
         this.preguntaPresion = preguntaPresion;
-        this.horarioColaborar = horarioColaborar;
+        this.participacionCapacitacionesIniciales = participacionCapacitacionesIniciales;
     }
 
-    // getters y setters
+    // getters
     public String getPreguntaMotivacion() { return preguntaMotivacion; }
-    public void setPreguntaMotivacion(String preguntaMotivacion) { this.preguntaMotivacion = preguntaMotivacion; }
-
     public String getPreguntaAporte() { return preguntaAporte; }
-    public void setPreguntaAporte(String preguntaAporte) { this.preguntaAporte = preguntaAporte; }
-
     public String getPreguntaPresion() { return preguntaPresion; }
-    public void setPreguntaPresion(String preguntaPresion) { this.preguntaPresion = preguntaPresion; }
-
-    public String getHorarioColaborar() { return horarioColaborar; }
-    public void setHorarioColaborar(String horarioColaborar) { this.horarioColaborar = horarioColaborar; }
+    public boolean isParticipacionCapacitacionesIniciales() { return participacionCapacitacionesIniciales; }
 }
