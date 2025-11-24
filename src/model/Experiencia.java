45b57b0f54e514ba06f97877d5b54fddcf3c1a07
fabriciojosesono10-star <fabriciojosesono10@gmail.com>
2025-11-24 13:12:11 -0500

    Update Experiencia.java

--- src/model/Experiencia.java
+++ src/model/Experiencia.java
@@ -1,26 +1,41 @@
 package model;
 
 public class Experiencia {
+    private boolean primAuxilios;
+    private boolean rcp;
+    private boolean controlHemorragias;
+    private boolean inmovilizacionFracturas;
+    private boolean evacuacionHeridos;
+    private boolean busquedaRescate;
+    private boolean rescateVehicular;
+    private boolean rescateAcuatico;
+    private boolean rescateAlturas;
+    private boolean supervivenciaCampoSelva;
 
-    private boolean voluntarioAnteriormente;
-    private String experienciaPrevia;
-    private String participacionEnProgramas;
-
-    public Experiencia() {}
-
-    public Experiencia(boolean voluntarioAnteriormente, String experienciaPrevia, String participacionEnProgramas) {
-        this.voluntarioAnteriormente = voluntarioAnteriormente;
-        this.experienciaPrevia = experienciaPrevia;
-        this.participacionEnProgramas = participacionEnProgramas;
+    public Experiencia(boolean primAuxilios, boolean rcp, boolean controlHemorragias, boolean inmovilizacionFracturas,
+                       boolean evacuacionHeridos, boolean busquedaRescate, boolean rescateVehicular,
+                       boolean rescateAcuatico, boolean rescateAlturas, boolean supervivenciaCampoSelva) {
+        this.primAuxilios = primAuxilios;
+        this.rcp = rcp;
+        this.controlHemorragias = controlHemorragias;
+        this.inmovilizacionFracturas = inmovilizacionFracturas;
+        this.evacuacionHeridos = evacuacionHeridos;
+        this.busquedaRescate = busquedaRescate;
+        this.rescateVehicular = rescateVehicular;
+        this.rescateAcuatico = rescateAcuatico;
+        this.rescateAlturas = rescateAlturas;
+        this.supervivenciaCampoSelva = supervivenciaCampoSelva;
     }
 
-    // getters y setters
-    public boolean isVoluntarioAnteriormente() { return voluntarioAnteriormente; }
-    public void setVoluntarioAnteriormente(boolean voluntarioAnteriormente) { this.voluntarioAnteriormente = voluntarioAnteriormente; }
-
-    public String getExperienciaPrevia() { return experienciaPrevia; }
-    public void setExperienciaPrevia(String experienciaPrevia) { this.experienciaPrevia = experienciaPrevia; }
-
-    public String getParticipacionEnProgramas() { return participacionEnProgramas; }
-    public void setParticipacionEnProgramas(String participacionEnProgramas) { this.participacionEnProgramas = participacionEnProgramas; }
+    // getters
+    public boolean isPrimAuxilios() { return primAuxilios; }
+    public boolean isRcp() { return rcp; }
+    public boolean isControlHemorragias() { return controlHemorragias; }
+    public boolean isInmovilizacionFracturas() { return inmovilizacionFracturas; }
+    public boolean isEvacuacionHeridos() { return evacuacionHeridos; }
+    public boolean isBusquedaRescate() { return busquedaRescate; }
+    public boolean isRescateVehicular() { return rescateVehicular; }
+    public boolean isRescateAcuatico() { return rescateAcuatico; }
+    public boolean isRescateAlturas() { return rescateAlturas; }
+    public boolean isSupervivenciaCampoSelva() { return supervivenciaCampoSelva; }
 }
