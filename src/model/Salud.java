2902e161c5b1ec30261e1bd009c7ce9ed4316a5c
fabriciojosesono10-star <fabriciojosesono10@gmail.com>
2025-11-24 13:11:55 -0500

    Update Salud.java

--- src/model/Salud.java
+++ src/model/Salud.java
@@ -1,36 +1,24 @@
 package model;
 
 public class Salud {
-
     private boolean entrenamientoRegular;
     private boolean natacion;
     private boolean escalada;
+    private boolean marcha;
     private boolean tareasCargaPeso;
-    private String impedimentoFisico;
 
-    public Salud() {}
-
-    public Salud(boolean entrenamientoRegular, boolean natacion, boolean escalada, boolean tareasCargaPeso, String impedimentoFisico) {
+    public Salud(boolean entrenamientoRegular, boolean natacion, boolean escalada, boolean marcha, boolean tareasCargaPeso) {
         this.entrenamientoRegular = entrenamientoRegular;
         this.natacion = natacion;
         this.escalada = escalada;
+        this.marcha = marcha;
         this.tareasCargaPeso = tareasCargaPeso;
-        this.impedimentoFisico = impedimentoFisico;
     }
 
-    // getters y setters
+    // getters
     public boolean isEntrenamientoRegular() { return entrenamientoRegular; }
-    public void setEntrenamientoRegular(boolean entrenamientoRegular) { this.entrenamientoRegular = entrenamientoRegular; }
-
     public boolean isNatacion() { return natacion; }
-    public void setNatacion(boolean natacion) { this.natacion = natacion; }
-
     public boolean isEscalada() { return escalada; }
-    public void setEscalada(boolean escalada) { this.escalada = escalada; }
-
+    public boolean isMarcha() { return marcha; }
     public boolean isTareasCargaPeso() { return tareasCargaPeso; }
-    public void setTareasCargaPeso(boolean tareasCargaPeso) { this.tareasCargaPeso = tareasCargaPeso; }
-
-    public String getImpedimentoFisico() { return impedimentoFisico; }
-    public void setImpedimentoFisico(String impedimentoFisico) { this.impedimentoFisico = impedimentoFisico; }
 }
