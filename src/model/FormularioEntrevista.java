18a14425ff8a2009ee64f496c526943a692f4a86
fabriciojosesono10-star <fabriciojosesono10@gmail.com>
2025-11-24 13:11:26 -0500

    Update FormularioEntrevista.java

--- src/model/FormularioEntrevista.java
+++ src/model/FormularioEntrevista.java
@@ -1,31 +1,22 @@
 package model;
 
 public class FormularioEntrevista {
-
-    private Voluntario voluntario;
-    
-    // Secciones del formulario
     private DatosPersonales datosPersonales;
     private Salud salud;
     private Experiencia experiencia;
     private Motivacion motivacion;
 
-    public FormularioEntrevista(Voluntario voluntario) {
-        this.voluntario = voluntario;
+    public FormularioEntrevista(DatosPersonales datosPersonales, Salud salud,
+                                Experiencia experiencia, Motivacion motivacion) {
+        this.datosPersonales = datosPersonales;
+        this.salud = salud;
+        this.experiencia = experiencia;
+        this.motivacion = motivacion;
     }
 
-    public Voluntario getVoluntario() { return voluntario; }
-    public void setVoluntario(Voluntario voluntario) { this.voluntario = voluntario; }
-
+    // getters
     public DatosPersonales getDatosPersonales() { return datosPersonales; }
-    public void setDatosPersonales(DatosPersonales datosPersonales) { this.datosPersonales = datosPersonales; }
-
     public Salud getSalud() { return salud; }
-    public void setSalud(Salud salud) { this.salud = salud; }
-
     public Experiencia getExperiencia() { return experiencia; }
-    public void setExperiencia(Experiencia experiencia) { this.experiencia = experiencia; }
-
     public Motivacion getMotivacion() { return motivacion; }
-    public void setMotivacion(Motivacion motivacion) { this.motivacion = motivacion; }
 }
